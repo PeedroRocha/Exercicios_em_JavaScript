a81077694c5ef8aa435cde1385a67c3eaf1fe3b0
PeedroRocha <136763248+PeedroRocha@users.noreply.github.com>
2024-04-17 06:47:08 -0300

    Finalizando o exercício 43
    Finalizando o exercício 43

--- Exercicios - do curso de algoritmos/Ex043/ex043.js
+++ Exercicios - do curso de algoritmos/Ex043/ex043.js
@@ -1,14 +1,11 @@
 function contar() {
     res.innerHTML += `Contando: `
     for (let c = 30; c >= 1; c = c - 1) {
-        if (c % 2 = 0 ) {  // tentando usar o If e Else dentro do for
-            res.innerHTML += `${c}`
+        if (c % 4 == 0 ) { 
+            res.innerHTML += ` [${c}],`
         } else {
-            res.innerHTML += `${[c]}`
+            res.innerHTML += ` ${c},`
         }
-            
-        
-          
     }
     res.innerHTML += ` Acabou!`
 }
