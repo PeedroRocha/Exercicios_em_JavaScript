6f6ee9ec4e49280286e8c6ad6f8655bc0b6dc444
PeedroRocha <136763248+PeedroRocha@users.noreply.github.com>
2024-04-18 06:49:12 -0300

    Exercício 44
    Criando o exercício 44

---
+++ Exercicios - do curso de algoritmos/Ex044/ex044.js
@@ -0,0 +1,10 @@
+var i_txt = document.querySelector('#i_txt')
+var f_txt = document.querySelector('#f_txt')
+var inc_txt = document.querySelector('#inc_txt')
+var i = Number(i_txt.value)
+var f = Number(f_txt.value)
+var inc = Number(inc_txt.value)
+
+function contar() {
+    
+}
