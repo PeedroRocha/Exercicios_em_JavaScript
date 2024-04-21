c728d50ced6909458b6a1ed090bb60954e8cb069
PeedroRocha <136763248+PeedroRocha@users.noreply.github.com>
2024-04-21 15:59:49 -0300

    Update ex044.js
    Finalizado exercício 44 e 45

--- Exercicios - do curso de algoritmos/Ex044/ex044.js
+++ Exercicios - do curso de algoritmos/Ex044/ex044.js
@@ -1,14 +1,23 @@
-var i_txt = document.querySelector('#i_txt')
-var f_txt = document.querySelector('#f_txt')
-var inc_txt = document.querySelector('#inc_txt')
-var i = Number(i_txt.value)
-var f = Number(f_txt.value)
-var inc = Number(inc_txt.value)
+
 
 function contar() {
-   res.innerHTML += `Contando...` 
-   for (var num = i; num <= f; num = (num + inc) ) {
-        res.innerHTML += ` ${num} ,`
+   var i_txt = document.querySelector('#i_txt')
+   var f_txt = document.querySelector('#f_txt')
+   var inc_txt = document.querySelector('#inc_txt')
+   var i = Number(i_txt.value)
+   var f = Number(f_txt.value)
+   var inc = Number(inc_txt.value)
+   res.innerHTML += `Contagem: `
+   if ( i < f ) {
+      for (var cont = i; cont <= f ; cont = cont + inc ) 
+         res.innerHTML += ` ${cont} ,`
+   } else {
+      for (var cont = i; cont >= f ; cont = cont - inc ) {
+         res.innerHTML += ` ${cont} ,`
+   }
+  
    }
    res.innerHTML += ` Acabou!`
 }
+
+// Neste código temos o exercício 44 e 45.
