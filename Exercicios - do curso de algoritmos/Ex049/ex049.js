1de78104b0b69f57728796219a74c5ff0879a010
PeedroRocha <136763248+PeedroRocha@users.noreply.github.com>
2024-04-23 06:44:56 -0300

    Ex 049
    Criando o ex049

---
+++ Exercicios - do curso de algoritmos/Ex049/ex049.js
@@ -0,0 +1,15 @@
+function somar() {
+    var n1_txt = document.querySelector("#n1_txt")
+    var n2_txt = document.querySelector("#n2_txt")
+    var n3_txt = document.querySelector("#n3_txt")
+    var n4_txt = document.querySelector("#n4_txt")
+    var n5_txt = document.querySelector("#n5_txt")
+    var n6_txt = document.querySelector("#n6_txt")
+    var n1 = Number(n1_txt.value)
+    var n2 = Number(n2_txt.value)
+    var n3 = Number(n3_txt.value)
+    var n4 = Number(n4_txt.value)
+    var n5 = Number(n5_txt.value)
+    var n6 = Number(n6_txt.value)
+   
+}
