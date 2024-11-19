18cc2f34a77e854df65ea56783f68645f7c7da82
PeedroRocha <136763248+PeedroRocha@users.noreply.github.com>
2024-11-18 21:21:46 -0300

    Ultimo update

--- Exercicios - do curso de algoritmos/Ex049/ex049.js
+++ Exercicios - do curso de algoritmos/Ex049/ex049.js
@@ -11,6 +11,11 @@ function somar() {
     var n4 = Number(n4_txt.value)
     var n5 = Number(n5_txt.value)
     var n6 = Number(n6_txt.value)
+    var cont = 0
+    var par = 0
+    var inpar = 0
+    // continuação do exercício(acredito que devemos usar o for, como visto na aula 14)...
     var soma = n1 + n2 + n3 + n4 + n5 + n6 
     res.innerHTML += `${soma}`
+
 }
